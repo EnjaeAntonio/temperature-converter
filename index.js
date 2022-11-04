ab81f80d55e8ef0dc66e0ccf5b219fce092fae92
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-04 09:43:41 -0500

    finished product

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+// Utility Functions 
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+// Select HTML element by class, id and html element
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+const enterNumber = select('.enternumber');
+const checked = select('.check');
+const result = select('.result');
+const output = select('.output p');
+
+onEvent('click', result, () => {
+
+    let a = enterNumber.value;
+    if(!isNaN(a)){
+
+    } if (checked.checked) {
+        let toC = (a - 32) * 5 / 9;
+        output.innerText = `${a}F = ${toC.toFixed(2)}C`
+
+    } else {
+        let toF = a * 1.8 + 32; 
+        output.innerText = `${a}C = ${toF.toFixed(2)}F`
+       
+    } 
+
+});
+
+
+
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+
